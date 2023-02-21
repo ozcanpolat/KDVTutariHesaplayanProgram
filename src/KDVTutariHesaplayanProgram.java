4170e656e235d35380f68e5cd5b77e802ca1c513
ozcanpolat <93453642+ozcanpolat@users.noreply.github.com>
2023-02-21 04:56:49 +0300

    KDVTutariHesaplayanProgram

---
+++ src/KDVTutariHesaplayanProgram.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class KDVTutariHesaplayanProgram {
+    public static void main(String[] args) {
+        double tutar,kdvOran,kdvTutar,kdvliTutar;
+        Scanner input= new Scanner(System.in);
+        System.out.print("Ucret tutarini girinz: ");
+        tutar=input.nextDouble();
+
+        kdvOran = (tutar>0) && (tutar<1000) ? 0.18 : 0.08;
+
+        kdvTutar=tutar*kdvOran;
+        kdvliTutar=tutar+kdvTutar;
+
+        System.out.println("KDV'siz Tutar ="+tutar);
+        System.out.println("KDV Orani ="+kdvOran);
+        System.out.println("KDV Tutari ="+kdvTutar);
+        System.out.println("KDV'li Tutar ="+kdvliTutar);
+
+
+    }
+}
